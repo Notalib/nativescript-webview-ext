c8f2b930844465c957565f5bd83741a1f4730c9b
Morten Anton Bach Sjøgren <msn@nota.dk>
2018-05-04 21:51:46 +0200

    fix: pack.sh didn't work anymore

--- publish/pack.sh
+++ publish/pack.sh
@@ -1,48 +1,59 @@
 #!/bin/bash
+set -e
 
-SOURCE_DIR=../src;
-TO_SOURCE_DIR=src;
-PACK_DIR=package;
-ROOT_DIR=..;
-PUBLISH=--publish
+wd() {
+    cd $(dirname $0)
+    echo $PWD
+}
 
 install(){
     npm i
 }
 
-pack() {
+WORKDIR=$(wd)
+SOURCE_DIR="${WORKDIR}/../src";
+TO_SOURCE_DIR="${WORKDIR}/src";
+PACK_DIR="${WORKDIR}/package";
+ROOT_DIR="${WORKDIR}/..";
+PUBLISH=--publish
 
+cd $WORKDIR;
+
+pack() {
     echo 'Clearing /src and /package...'
-    node_modules/.bin/rimraf "$TO_SOURCE_DIR"
-    node_modules/.bin/rimraf "$PACK_DIR"
+    cd $WORKDIR
+    npx rimraf "$TO_SOURCE_DIR"
+    npx rimraf "$PACK_DIR"
 
     # copy src
     echo 'Copying src...'
-    git clean -d -f -x "$SOURCE_DIR" && (cd $SOURCE_DIR && npm i && npm run apply-patches)
-    node_modules/.bin/ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
+    git clean -d -f -x "$SOURCE_DIR" && (cd $SOURCE_DIR && npm i)
+    npx ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
 
     # copy README & LICENSE to src
     echo 'Copying README and LICENSE to /src...'
-    node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
-    node_modules/.bin/ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
+    npx ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
+    npx ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
 
     # compile package and copy files required by npm
     echo 'Building /src...'
     cd "$TO_SOURCE_DIR"
-    node_modules/.bin/tsc
-    cd ..
+    npm run build
+    cd $WORKDIR
 
     echo 'Creating package...'
     # create package dir
     mkdir "$PACK_DIR"
 
+    npx json -e "delete this.scripts" -I -f "$TO_SOURCE_DIR"/package.json
+
     # create the package
     cd "$PACK_DIR"
-    npm pack ../"$TO_SOURCE_DIR"
+    npm pack "$TO_SOURCE_DIR"
 
     # delete source directory used to create the package
-    cd ..
-    node_modules/.bin/rimraf "$TO_SOURCE_DIR"
+    cd $WORKDIR
+    npx rimraf "$TO_SOURCE_DIR"
 }
 
 install && pack
