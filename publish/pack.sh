e6851be73fcae34bcc15279204fe9fd3c1d782b8
Morten Anton Bach Sjøgren <msn@nota.dk>
2018-05-22 12:54:13 +0200

    chor: minor update to pack.sh

--- publish/pack.sh
+++ publish/pack.sh
@@ -21,45 +21,50 @@ cd $WORKDIR;
 
 pack() {
     echo 'Clearing /src and /package...'
-    cd $WORKDIR
-    npx rimraf "$TO_SOURCE_DIR"
-    npx rimraf "$PACK_DIR"
+    cd "${WORKDIR}"
+    npx rimraf "${TO_SOURCE_DIR}"
+    npx rimraf "${PACK_DIR}"
 
     # copy src
     echo 'Copying src...'
-    git clean -d -f -x "$SOURCE_DIR" && (cd $SOURCE_DIR && npm i)
-    npx ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
+    git clean -d -f -x "${SOURCE_DIR}" && (cd "${SOURCE_DIR}" && npm i)
+    npx ncp "${SOURCE_DIR}" "${TO_SOURCE_DIR}"
 
     # copy README & LICENSE to src
     echo 'Copying README and LICENSE to /src...'
-    npx ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
-    npx ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
+    npx ncp "${ROOT_DIR}"/LICENSE "${TO_SOURCE_DIR}"/LICENSE
+    npx ncp "${ROOT_DIR}"/README.md "${TO_SOURCE_DIR}"/README.md
 
     # compile package and copy files required by npm
     echo 'Building /src...'
-    cd "$TO_SOURCE_DIR"
+    cd "${TO_SOURCE_DIR}"
     npm run build
-    cd $WORKDIR
+    cd "${WORKDIR}"
 
+    if [[ ! -f "${TO_SOURCE_DIR}/www/ns-webview-bridge.js" ]]; then
+        echo "${TO_SOURCE_DIR}/www/ns-webview-bridge.js is missing";
+        exit 127
+    fi
 
-    if [[ ! -f "$TO_SOURCE_DIR/www/ns-webview-bridge.js" ]]; then
-        echo "$TO_SOURCE_DIR/www/ns-webview-bridge.js is missing";
+    if [[ ! -f "${TO_SOURCE_DIR}/www/promise-polyfill.js" ]]; then
+        echo "${TO_SOURCE_DIR}/www/promise-polyfill.js is missing";
         exit 127
     fi
 
     echo 'Creating package...'
     # create package dir
-    mkdir "$PACK_DIR"
+    mkdir "${PACK_DIR}"
 
-    npx json -e "delete this.scripts" -I -f "$TO_SOURCE_DIR"/package.json
+    npx json -e "delete this.scripts" -I -f "${TO_SOURCE_DIR}"/package.json
+    npx json -e "delete this.devDependencies" -I -f "${TO_SOURCE_DIR}"/package.json
 
     # create the package
-    cd "$PACK_DIR"
-    npm pack "$TO_SOURCE_DIR"
+    cd "${PACK_DIR}"
+    npm pack "${TO_SOURCE_DIR}"
 
     # delete source directory used to create the package
-    cd $WORKDIR
-    npx rimraf "$TO_SOURCE_DIR"
+    cd "${WORKDIR}"
+    npx rimraf "${TO_SOURCE_DIR}"
 }
 
 install && pack
