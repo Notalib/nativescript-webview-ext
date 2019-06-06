26321282d6bbb58454fdc704e0e9ed5ddb95c44b
Morten Sjøgren <m_abs@mabs.dk>
2019-06-06 22:13:39 +0200

    fix: typings

--- src/webview-ext-common.ts
+++ src/webview-ext-common.ts
@@ -322,12 +322,6 @@ export class UnsupportedSDKError extends Error {
     }
 }
 
-declare module "url" {
-    interface UrlWithStringQuery extends Url {
-        format(): string;
-    }
-}
-
 @CSSType("WebView")
 export class WebViewExtBase extends ContainerView {
     /**
@@ -573,7 +567,6 @@ export class WebViewExtBase extends ContainerView {
             object: this,
             url,
         } as ShouldOverrideUrlLoadEventData;
-
         this.notify(args);
 
         const eventNameWithSpellingError = "shouldOverideUrlLoading";
@@ -1503,4 +1496,33 @@ export interface IOSWebViewWrapper {
 
     scrollBounce: boolean;
     scalesPageToFit: boolean;
+}
+
+declare module "url" {
+    export interface ParsedUrlQuery {
+        [key: string]: string | string[];
+    }
+
+    export class Url {
+        protocol?: string | null;
+        slashes?: string | null;
+        auth?: string | null;
+        host?: string | null;
+        port?: string | null;
+        hostname?: string | null;
+        hash?: string | null;
+        search?: string | null;
+        query?: string | null | ParsedUrlQuery;
+        pathname?: string | null;
+        path?: string | null;
+        href?: string | null;
+        public format(): string;
+        public resolve(relative: Url): string;
+        public resolveObject(source: string, relative: string): Url;
+    }
+
+    export function parse(input: string): Url;
+    export function format(url: Url): string;
+    export function resolve(source: Url, relative: Url): string;
+    export function resolveObject(source: string, relative: string): Url;
 }
