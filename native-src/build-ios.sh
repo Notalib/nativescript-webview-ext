8b0d1536ea38962f9afe1a468702b53eeeec65da
Morten Sjøgren <m_abs@mabs.dk>
2020-12-23 12:32:02 +0100

    fix: xcode-12.3 build

--- native-src/build-ios.sh
+++ native-src/build-ios.sh
@@ -5,21 +5,61 @@ set -e
 # Based on https://appspector.com/blog/xcframeworks
 
 ROOT_DIR="$(git rev-parse --show-toplevel)"
-NOTA_WEBVIEW_DIR="${ROOT_DIR}/native-src/ios/NotaWebViewExt"
-TARGET="${ROOT_DIR}/src/platforms/ios/NotaWebViewExt.xcframework"
 
-cd $NOTA_WEBVIEW_DIR
+PROJECT_NAME="NotaWebViewExt"
 
-rm -rf $TARGET
+# set framework folder name
+FRAMEWORK_FOLDER_NAME="${PROJECT_NAME}"
 
-# Archive for iOS
-xcodebuild archive -scheme NotaWebViewExt -destination="iOS" -archivePath /tmp/xcf/ios.xcarchive -derivedDataPath /tmp/iphoneos -sdk iphoneos SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+IOS_PROJECT_DIR="${ROOT_DIR}/native-src/ios/${FRAMEWORK_FOLDER_NAME}"
 
-# Archive for simulator
-xcodebuild archive -scheme NotaWebViewExt -destination="iOS Simulator" -archivePath /tmp/xcf/iossimulator.xcarchive -derivedDataPath /tmp/iphoneos -sdk iphonesimulator SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+cd ${IOS_PROJECT_DIR}
 
-# Build xcframework with two archives
-xcodebuild -create-xcframework -framework /tmp/xcf/ios.xcarchive/Products/Library/Frameworks/NotaWebViewExt.framework -framework /tmp/xcf/iossimulator.xcarchive/Products/Library/Frameworks/NotaWebViewExt.framework -output $TARGET
+# set framework name or read it from project by this variable
+FRAMEWORK_NAME="${PROJECT_NAME}"
+
+# xcframework path
+FRAMEWORK_PATH="${ROOT_DIR}/src/platforms/ios/${FRAMEWORK_NAME}.xcframework"
+
+TMP_DIR="/tmp/xcf"
+
+# set path for iOS simulator archive
+SIMULATOR_ARCHIVE_PATH="${TMP_DIR}/${FRAMEWORK_FOLDER_NAME}/simulator.xcarchive"
+
+# set path for iOS device archive
+IOS_DEVICE_ARCHIVE_PATH="${TMP_DIR}/${FRAMEWORK_FOLDER_NAME}/iOS.xcarchive"
+
+ARCHIVE_SUBPATH="Products/Library/Frameworks/${FRAMEWORK_NAME}.framework"
+
+rm -rf "${TMP_DIR}" "${FRAMEWORK_PATH}"
+
+echo "Deleted ${TMP_DIR}"
+
+echo "Archiving ${FRAMEWORK_NAME}"
+
+xcodebuild archive \
+  -scheme "${FRAMEWORK_NAME}" \
+  -destination="generic/platform=iOS Simulator" \
+  -archivePath "${SIMULATOR_ARCHIVE_PATH}" \
+  -sdk iphonesimulator \
+  SKIP_INSTALL=NO \
+  BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+xcodebuild archive \
+  -scheme "${FRAMEWORK_FOLDER_NAME}" \
+  -destination="generic/platform=iOS" \
+  -archivePath "${IOS_DEVICE_ARCHIVE_PATH}" \
+  -sdk iphoneos \
+  SKIP_INSTALL=NO \
+  BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+
+# Creating XCFramework
+xcodebuild -create-xcframework \
+  -framework "${SIMULATOR_ARCHIVE_PATH}/${ARCHIVE_SUBPATH}" \
+  -framework "${IOS_DEVICE_ARCHIVE_PATH}/${ARCHIVE_SUBPATH}" \
+  -output "${FRAMEWORK_PATH}"
+
+rm -rf "${SIMULATOR_ARCHIVE_PATH}" "${IOS_DEVICE_ARCHIVE_PATH}"
 
 ## Workaround for https://github.com/Notalib/nativescript-webview-ext/issues/71
-rm -rf ${TARGET}/ios-armv7_arm64/NotaWebViewExt.framework/Frameworks
+rm -rf ${FRAMEWORK_PATH}/ios-*/NotaWebViewExt.framework/Frameworks
