4b2aa6a300d4dec8e164b591146d175a0c98768f
Morten Anton Bach Sjøgren <msn@nota.dk>
2021-04-22 11:25:15 +0200

    fix: demo

--- demo/references.d.ts
+++ demo/references.d.ts
@@ -1,2 +1,1 @@
-/// <reference path="../src/node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="../src/node_modules/@nativescript/types-android/lib/android-29.d.ts" />
+/// <reference path="./node_modules/@nativescript/types/index.d.ts" />
